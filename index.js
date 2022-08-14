5cdd9aba0d5594439ec2e305218e379687d759a7
Andrey <andrey_34rus@mail.ru>
2022-08-14 16:16:24 +0300

    test to server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/sqlAdd', require('./routes/sql.js'));
+
+app.listen(port, () => {
+   console.log('server node_counter started on localhost:' + port);
+});
